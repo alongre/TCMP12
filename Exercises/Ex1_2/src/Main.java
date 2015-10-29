b2a9fc85fe7daa2b6daa67500e5b245e1e2dcacc
Alon Green <alongre@gmail.com>
2015-10-29 18:43:35 +0200

    Ex1_2 completed

--- Exercises/Ex1_2/src/Main.java
+++ Exercises/Ex1_2/src/Main.java
@@ -3,7 +3,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Person [] persons = new Person[3];
+		persons[0] = new Person("043243455");
+		persons[1] = new Person("43434343","Alon","Nahariyya",37);
+		persons[2] = new Person("34234","Tom","Nahariyya",3);
+		
+		persons[0].setAge(25);
+		persons[0].setName("Yuval");
+		persons[0].setAddress("Haifa");
+		
+		for (Person person : persons) {
+			System.out.println(person.toString());
+		}
 	}
 
 }
--- Exercises/Ex1_2/src/Person.java
+++ Exercises/Ex1_2/src/Person.java
@@ -5,4 +5,78 @@ public class Person {
 	private String name;
 	private String address;
 	private int age;
+	
+	/**
+	 * @return name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * Get address
+	 * @return address
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * Set address
+	 * @param address
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	/**
+	 * @return age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	
+	
+	/**
+	 * @param id
+	 * @param name
+	 * @param address
+	 * @param age
+	 */
+	public Person(String id, String name, String address, int age) {
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.age = age;
+	}
+
+	/**
+	 * @param id
+	 */
+	public Person(String id) {
+	
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", address=" + address + ", age=" + age + "]";
+	}
+	
+	
 }
