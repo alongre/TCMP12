5b0cf82bfa068c65ef3160b13a50a9311a7e67b2
alongre <alongre@gmail.com>
2015-10-31 20:34:52 +0200

    Completed Ex1_4

--- Exercises/Ex1/Ex1_4/src/Rectangle.java
+++ Exercises/Ex1/Ex1_4/src/Rectangle.java
@@ -1,65 +1,66 @@
+import java.awt.Point;
 
 public class Rectangle {
 
-	private double x = 0;
-	private double y = 0;
-	private double length = 0;
-	private double width = 0;
+	private int x = 0;
+	private int y = 0;
+	private int height = 0;
+	private int width = 0;
 	
 	
 	/**
 	 * @return the x
 	 */
-	public double getX() {
+	public int getX() {
 		return x;
 	}
 
 	/**
-	 * @param x the x to set
+	 * @param the x to set
 	 */
-	public void setX(double x) {
+	public void setX(int x) {
 		this.x = x;
 	}
 
 	/**
 	 * @return the y
 	 */
-	public double getY() {
+	public int getY() {
 		return y;
 	}
 
 	/**
 	 * @param y the y to set
 	 */
-	public void setY(double y) {
+	public void setY(int y) {
 		this.y = y;
 	}
 
 	/**
-	 * @return the length
+	 * @return the height
 	 */
-	public double getLength() {
-		return length;
+	public int getHeight() {
+		return height;
 	}
 
 	/**
-	 * @param length the length to set
+	 * @param  the height to set
 	 */
-	public void setLength(double length) {
-		this.length = length;
+	public void setHeight(int height) {
+		this.height = height;
 	}
 
 	/**
 	 * @return the width
 	 */
-	public double getWidth() {
+	public int getWidth() {
 		return width;
 	}
 
 	/**
 	 * @param width the width to set
 	 */
-	public void setWidth(double width) {
+	public void setWidth(int width) {
 		this.width = width;
 	}
 
@@ -69,10 +70,10 @@ public class Rectangle {
 	 * @param length
 	 * @param width
 	 */
-	public Rectangle(double x, double y, double length, double width) {
+	public Rectangle(int x, int y, int height, int width) {
 		this.x = x;
 		this.y = y;
-		this.length = length;
+		this.height = height;
 		this.width = width;
 	}
 	
@@ -81,8 +82,8 @@ public class Rectangle {
 	 * @param length
 	 * @param width
 	 */
-	public Rectangle(double length, double width) {
-		this.length = length;
+	public Rectangle(int height, int width) {
+		this.height = height;
 		this.width = width;
 	}
 	
@@ -90,8 +91,68 @@ public class Rectangle {
 	
 	public int area()
 	{
-		Double rectArea = length * width;
-		return rectArea.intValue();
+		return height * width;
+	}
+	
+	public int perimeter()
+	{
+		return height*2 +  width*2;
+	}
+	public void resize(int percent)
+	{
+		double p = percent * 1.0 / 100;
+		
+		//double mult = percent/100;
+		
+		height*= p;
+		width*= p;
+	}
+	public void resize(int w,int h)
+	{
+		width = w;
+		height = h;
+	}
+
+	public boolean isEqual(Rectangle r)
+	{
+		if (this.area() == r.area()) return true;
+		return false;
+	}
+	public void move(int x,int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+	public void move(Point p)
+	{
+		if (p==null) return;
+		this.x = p.x;
+		this.y = p.y;
+	}
+	
+	public Rectangle Union(Rectangle r)
+	{
+		if (r == null) return this;
+		int xLeft = Math.min(this.x, r.x);
+		int yBottom = Math.min(this.y, r.y);
+		int xRight = Math.max(this.x + this.width, r.x + r.width);
+		int yTop = Math.max(this.y + height, r.y + r.height);
+		return new Rectangle(xLeft,yBottom,yTop - yBottom,xRight - xLeft);
+	}
+	public boolean isIntersect(Rectangle r)
+	{
+		if (x+width < r.x || r.x+r.width < x || y+height < r.y || r.y+r.height<y)
+			return false;
+		return true;
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Rectangle [x=" + x + ", y=" + y + ", height=" + height + ", width=" + width + "]";
 	}
 	
 }
