5ec35a0ac6a55766b7464646b2ec605f578a8d46
alongre <alongre@gmail.com>
2015-10-31 23:57:30 +0200

    Added Ex1_6

---
+++ Exercises/Ex1/Ex1_6/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter the size of the polygon:");
+		int polygonSize = sc.nextInt();
+		for (int i=0;i<polygonSize;i++)
+		{
+			System.out.println("Please enter point number: " + (i+1));
+			System.out.println("x value:");
+			sc.nextLine();
+			int tmpX = sc.nextInt();
+			System.out.println("y value:");
+			int tmpY = sc.nextInt();
+		}
+	}
+
+}
---
+++ Exercises/Ex1/Ex1_6/src/Polygon.java
@@ -0,0 +1,13 @@
+import java.awt.Point;
+
+public class Polygon {
+	Point [] points;
+
+	/**
+	 * @param points
+	 */
+	public Polygon(int size) {
+		this.points = new Point[size];
+	}
+	
+}
