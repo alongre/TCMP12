aec480024de076e8741e989f1fec8c2808794870
alongre <alongre@gmail.com>
2015-11-07 19:50:21 +0200

    Update Ex2_1

--- Exercises/Ex2/Ex2_1/src/Main.java
+++ Exercises/Ex2/Ex2_1/src/Main.java
@@ -1,21 +1,23 @@
-import java.awt.Color;
+
 
 
 public class Main {
 
 	public static void main(String[] args) {
-		Shape[] shapes = new Shape[3];
+		IShape [] shapes = new Shape[3];
 		shapes[0] = new Rectangle(10,20,"Rect1");
-		shapes[1] = new Circle(new Point(30,20),Color.blue,false,"Circle1",15);
+		shapes[1] = new Circle("Circle1",10);
 		shapes[2] = new Line("Line1", new Point(5,0));
-		ShapeContainer shapeList = new ShapeContainer(shapes);
-		shapeList.add(new Circle(new Point(10,10),Color.red,true,"Circle2",10));
-		shapeList.add(new Line("Line2",new Point(30,10)));
 		
-		shapeList.drawAll();
-		shapeList.resizeAll(50);
-		System.out.println(shapeList.toString());
-		
+		for(IShape shape:shapes)
+		{
+			System.out.println("Shape name: " + shape.getName());
+			shape.draw();
+			System.out.println("Area is: " + shape.area());
+			System.out.println("Perimeter is: " + shape.perimeter());
+			shape.remove();
+			System.out.println("----------------------------");
+		}
 			
 	}
 
