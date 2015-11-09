213f3890e41e54658b2b4134730a3de0693aa0de
Alon Green <alongre@gmail.com>
2015-11-09 16:56:20 +0200

    Ex2_4 completed

--- Exercises/Ex2/Ex2_4/src/CustomerContainer.java
+++ Exercises/Ex2/Ex2_4/src/CustomerContainer.java
@@ -2,8 +2,11 @@
 public class CustomerContainer {
 
 	private Customer[] customersList;
+	private int index;
+	
 	public CustomerContainer (int size)
 	{
+		index = 0;
 		if (size>0)
 		{
 			customersList = new Customer[size];
@@ -12,42 +15,80 @@ public class CustomerContainer {
 
 	 public boolean add( Customer customer)
 	 {
-		 
+		 if (index<customersList.length && index>=0 && !contains(customer))
+		 {
+			 customersList[index++] = customer;
+			 return true;
+		 }
+		 return false;
 	 }
 	 private boolean contains(Customer customer)
 	 {
 		 for(Customer cust:customersList)
 		 {
+			 if (cust == null) break;
 			 if (cust.equals(customer)) return true;
 		 }
 		 return false;
 	 }
 	
-	 public void drawAll()
+	 public boolean remove( int arrayIndex)
 	 {
-		 
+		 if (arrayIndex>=0 && index<customersList.length)
+		 {
+			 //last item on the array
+			 if (arrayIndex == customersList.length-1)
+			 {
+				 customersList[index--] = null;
+				 return true;
+			 }
+			 for (int i = arrayIndex;i<customersList.length-1;i++)
+			 {
+				 customersList[i] =  customersList[i+1];
+			 }
+			 index--;
+			 return true;
+		 }
+		 return false;
 	 }
 
-	 public boolean remove( int index)
+	 public boolean remove( Customer customer)
 	 {
-		 
+		 if (index<customersList.length && index>=0 && contains(customer))
+		 {
+			 int locatedIndex = getIndex(customer);
+			 return remove(locatedIndex);
+		 }
+		 return false;
 	 }
-
-	 public boolean remove( Customer c)
+	 private int getIndex(Customer customer)
 	 {
-		 
+		 for (int arrayIndex=0;arrayIndex<customersList.length;arrayIndex++)
+		 {
+			 if (customersList[arrayIndex].equals(customer)) return arrayIndex;
+		 }
+		 return -1;
 	 }
-	 
 
 	 public boolean removeAll()
 	 {
-		 
+		 if (customersList.length == 0) return false;
+		 for (int index = 0; index<customersList.length;index++)
+		 {
+			 customersList[index] = null;
+		 }
+		 index = 0;
+		 return true;
 	 }
-
-	 public void resizeAll ( int percent )
+	 public void print()
 	 {
-		 
-	 }
-	
-	
+		for (Customer customer : customersList)
+		{
+			if (customer==null) break;
+			System.out.println(customer.toString());
+			System.out.println("*********************");
+		}
+		System.out.println("Array index: " + index);
+		System.out.println("Array Size: " + customersList.length);
+	}
 }
--- Exercises/Ex2/Ex2_4/src/Main.java
+++ Exercises/Ex2/Ex2_4/src/Main.java
@@ -7,15 +7,35 @@ public class Main {
 		// TODO Auto-generated method stub
 		Calendar cal = Calendar.getInstance();
 		cal.set(2015,Calendar.JULY, 29);
-		Customer [] customers = new Customer[3];
-		customers[0] = new Customer("Alon","Shaked st");
-		customers[0].addToPayment(100);
-		customers[1] = new RegCustomer("Roy","Shaked st",cal.getTime());
-		customers[1].addToPayment(500);
-		customers[2] = new VipCustomer("Tom","Shaked st",50);
-		customers[2].addToPayment(1000);
-		Customers customerList = new Customers(customers);
-		customerList.print();
+		Customer customer1 = new Customer("Alon","Shaked st");
+		customer1.addToPayment(100);
+		Customer customer2 = new RegCustomer("Roy","Shaked st",cal.getTime());
+		customer2.addToPayment(500);
+		Customer customer3 = new VipCustomer("Tom","Shaked st",50);
+		customer3.addToPayment(1000);
+		
+		
+		//Customers customerList = new Customers(customers);
+		
+		CustomerContainer ct = new CustomerContainer(5);
+		ct.add(customer1);
+		ct.add(customer2);
+		ct.add(customer3);
+		ct.print();
+		System.out.println("Removed index 1");
+		ct.remove(1);
+		ct.print();
+		System.out.println("added Roy again");
+		ct.add(customer2);
+		ct.print();
+		System.out.println("Removed alon");
+		ct.remove(customer1);
+		ct.print();
+		System.out.println("Removed all");
+		ct.removeAll();
+		ct.print();
+		
+		
 	}
 
 }
